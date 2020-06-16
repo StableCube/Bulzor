219a270b1c0479f617ec65f3db35a7d01649eaa6
Zachary Boyet <support@dainbramaged.com>
2020-06-16 11:12:51 -0700

    Accordion footer

---
+++ Components.Extended/src/Components/BulAccordionRowFooter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components;
+
+namespace StableCube.Bulzor.Components.Extended
+{
+    public class BulAccordionRowFooter : BulComponentBase
+    {
+        [CascadingParameter]
+        public bool Active { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected BulmaClassBuilder ClassBuilder { get; set; } = new BulmaClassBuilder("bul-accordion-row-footer");
+
+        protected string _elementClass = String.Empty;
+
+        protected override void BuildBulma()
+        {
+            ClassBuilder.SetIsActive(Active);
+            _elementClass = ClassBuilder.ToString();
+        }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            BuildBulma();
+
+            builder.OpenComponent<BulCardFooter>(0);
+            builder.AddMultipleAttributes(1, AdditionalAttributes);
+            builder.AddAttribute(2, "class", MergeClassAttribute(_elementClass));
+            builder.AddAttribute(3, "ChildContent", ChildContent);
+            builder.CloseComponent();
+        }
+    }
+}
