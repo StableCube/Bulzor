dc2e6a068796c8e715476291ef8499ff1ae3429e
zboyet <support@stablecube.com>
2022-01-09 10:01:48 -0800

    Removed unused code

--- Components.Extended/wwwroot/js/interop.js
+++ Components.Extended/wwwroot/js/interop.js
@@ -1,17 +1,3 @@
 (function() {
-    window.bulSetPageTitle = (value) => {
-        document.title = value;
-    };
 
-    window.bulSetMetaTag = (name, content) => {
-        var el = document.querySelector(`meta[name="${name}"]`);
-
-        if(!el) {
-            el = document.createElement('meta');
-            document.getElementsByTagName('head')[0].appendChild(el);
-            el.name = name;
-        }
-
-        el.setAttribute("content", content);
-    };
 })();
